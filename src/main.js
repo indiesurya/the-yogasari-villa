3331da08a552764a90b7847e3cca01975ea22b2b
indiesurya <putuindie11@gmail.com>
2022-11-17 09:47:04 +0800

    [17112022-09:46] Setup all needed in project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-import './assets/main.css'
+import './assets/css/bootstrap.css'
+import './assets/js/bootstrap.js'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
---
+++ src/views/pages/ReviewView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        Review
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
