d9638ba363c332ec3bcee5f980fd21ca46255565
indiesurya <putuindie11@gmail.com>
2022-11-21 19:36:01 +0800

    [21112022-1935] Add main navbar partials

--- src/views/partials/MainNavbar.vue
+++ src/views/partials/MainNavbar.vue
@@ -1,33 +1,31 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-light">
+  <nav class="navbar navbar-expand-lg">
     <div class="container">
       <a class="navbar-brand" href="#">The Yogasari Villa</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <router-link class="nav-link" :class="[activeNavbar == 'about' ? 'active' : '']" aria-current="page" to="/">About</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" :class="[activeNavbar == 'facilities' ? 'active' : '']" aria-current="page" to="/facilities">Facilities</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" :class="[activeNavbar == 'review' ? 'active': '']" to="/review">Review</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" :class="[activeNavbar == 'contact' ? 'active': '']" to="/contact">Contact Us</router-link>
-          </li>
-        </ul>
+      <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+      <span class="navbar-toggler-icon"></span>
+        </button>
+      <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">The Yogasari Villa</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <router-link class="nav-link" :class="[activeNavbar == 'about' ? 'active' : '']" aria-current="page" to="/">About Us</router-link>
+            </li>
+            <li class="nav-item">
+                <router-link class="nav-link" :class="[activeNavbar == 'facilities' ? 'active' : '']" aria-current="page" to="/facilities">Facilities</router-link>
+            </li>
+            <li class="nav-item">
+                <router-link class="nav-link" :class="[activeNavbar == 'review' ? 'active': '']" to="/review">Review</router-link>
+            </li>
+            <li class="nav-item">
+                <router-link class="nav-link" :class="[activeNavbar == 'contact' ? 'active': '']" to="/contact">Contact Us</router-link>
+            </li>
+            </ul>
+        </div>
       </div>
       </div>
   </nav>
