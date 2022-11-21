91a4345e267faa823a16b4efc3af5ff0125617eb
indiesurya <putuindie11@gmail.com>
2022-11-21 19:31:38 +0800

    [21112022-1932] Import dependencies

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "video.js/dist/video-js.css"
 
 import "./assets/css/custom.css"
 
